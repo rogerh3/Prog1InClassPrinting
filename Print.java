e256034f20c333c8eb69352ad2fe408b315db44e
rogerh3 <81320876+rogerh3@users.noreply.github.com>
2021-03-26 13:19:37 -0400

    Add files via upload

---
+++ Print.java
@@ -0,0 +1,20 @@
+//Project: Printing
+//Roger Hayden III
+//Description: Practice print commands
+//Version: 1.0
+//Last Update: 8/27/2020
+public class Print {
+
+		//main method
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Prints to the output
+		System.out.print("Hello World");
+		System.out.print(" Hi there\n");
+		System.out.println("line3");
+		System.out.println("\tRoger Hayden");
+	
+	}
+
+}
